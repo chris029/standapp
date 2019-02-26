ab300b2f7970e3bfc79d414181ae3bc1680c948a
krzysiek <krzysiek029@gmail.com>
2019-02-26 22:42:09 +0100

    Multiple screens

--- stand_app.py
+++ stand_app.py
@@ -3,15 +3,28 @@ from time_guard import TimeGuard
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 timer = TimeGuard(1)
 
 Builder.load_file('StandApp.kv')
 
-class Container(BoxLayout):
+class AnotherScreen(Screen):
+    pass
+    
+    
+    
+class ScreenManagement(ScreenManager):
+    pass
+    
+
+
+class Container(Screen):
     def AssignTime(self):
         timer.setTimeInt(self.ids.timeInput.text)
         self.ids.animation.text = "Not yet"
+        
+        
         
 class StandApp(App):
 
@@ -19,16 +32,19 @@ class StandApp(App):
         self.title = 'StandApp'
         Clock.schedule_interval(lambda dt: self.my_callback(), 10)
         
-        return Container()
+        return ScreenManagement()
      
     def my_callback(self):
         app = App.get_running_app()
         
         timer.CheckTime()
-        if(timer.getStatus()):
-            app.root.ids.animation.text = "Yeah, it works"
-        else:
-            app.root.ids.animation.text = "Not yet"
+        if(isinstance(app.root.current_screen, Container)):
+            if(timer.getStatus()):
+                app.root.current_screen.ids.animation.text = "Yeah, it works"
+            else:
+                app.root.current_screen.ids.animation.text = "Not yet"
+            
+         
             
 def main():
     app = StandApp()
